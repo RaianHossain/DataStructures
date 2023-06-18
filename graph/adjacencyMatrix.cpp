bac3ac992804f89acb0d5e76f43415bf2285a24c
RaianHossain <raiansanil@gmail.com>
2023-06-19 00:58:11 +0600

    bfs updated

--- graph/adjacencyMatrix.cpp
+++ graph/adjacencyMatrix.cpp
@@ -5,114 +5,104 @@ using namespace std;
 
 class Graph {
     private:
-    char vertices[MAX_SIZE];
-    bool edges[MAX_SIZE][MAX_SIZE] = {0};
-    int sizeOfV;
-    vector <bool> visitedDfs;
+        char vertices[MAX_SIZE];
+        bool edges[MAX_SIZE][MAX_SIZE] = {0};
+        int sizeOfV;        
 
     public:
+        Graph() {
+            sizeOfV = -1;
+        }
 
-    Graph() {
-        sizeOfV = -1;
-    }
-
-    void addVertex(char v) {
-        sizeOfV++;
-        vertices[sizeOfV] = v;
-        visitedDfs.push_back(false);
-        return;
-    }   
-
-    void addEdge(char src, char dest) {
-        int srcIndex = getIndex(src);
-        int destIndex = getIndex(dest);
-        if(srcIndex == -1 || destIndex == -1) {
-            cout<<"Invalid verteces"<<endl;
+        void addVertex(char v) {
+            sizeOfV++;
+            vertices[sizeOfV] = v;
+            visitedDfs.push_back(false);
             return;
         }
-        edges[srcIndex][destIndex] = true;
-        edges[destIndex][srcIndex] = true;
-        return;
-    }  
 
-    void printMatrix() {
-        for(int i = 0; i<= sizeOfV; i++) {
-            cout<<i<<"-"<<vertices[i]<<", ";            
-        }
-        cout<<endl;
-        for(int i = 0; i<= sizeOfV; i++) {
-            for(int j = 0; j<= sizeOfV; j++) {
-                cout<<edges[i][j]<<" ";
+        void addEdge(char src, char dest) {
+            int srcIndex = getIndex(src);
+            int destIndex = getIndex(dest);
+            if(srcIndex == -1 || destIndex == -1) {
+                cout<<"Invalid vertices"<<endl;
+                return;
             }
-            cout<<endl;
+            edges[srcIndex][destIndex] = true;
+            edges[destIndex][srcIndex] = true;
+            return;
         }
-        return;
-    }
 
-    int getIndex(char ch) {
-        for(int i = 0; i <= sizeOfV; i++) {
-            if(vertices[i] == ch) {
-                return i;
+        void printMatrix() {
+            for(int i = 0; i<= sizeOfV; i++) {
+                cout<<i<<"-"<<vertices[i]<<", ";
+            }
+            cout<<endl<<endl;
+            for(int i = 0; i<= sizeOfV; i++) {
+                for(int j = 0; j<= sizeOfV; j++) {
+                    cout<<edges[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            return;
+        }
+
+        int getIndex(char ch) {
+            for(int i = 0; i <= sizeOfV; i++) {
+                if(vertices[i] == ch) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void bfs(char sr)
+        {
+            vector<bool> visited(sizeOfV, false);
+            queue<int> q;
+
+            int level[sizeOfV];
+            char parent[sizeOfV];
+
+            int start = getIndex(sr);
+            q.push(start);
+
+            visited[start] = true;
+            level[0] = 0;
+            parent[0] = 0;
+
+            int currentNode;
+            while (!q.empty()) {
+                currentNode = q.front();
+                q.pop();
+
+                for (int i = 0; i <= sizeOfV; i++) {
+                    if (edges[currentNode][i] == 1 && (!visited[i])) {
+                        q.push(i);
+                        visited[i] = true;
+                        level[i] = level[currentNode] + 1;
+                        parent[i] = currentNode;
+                    }
+                }
+            }
+            for(int i = 0; i <= sizeOfV; i++) {
+                cout<<vertices[i]<<"-"<<level[i]<<"- Parent "<<vertices[parent[i]]<<endl;
             }
         }
-        return -1;
-    }
-
-    void bfs(char sr)
-    {
-        // Visited vector to so that
-        // a vertex is not visited more than once
-        // Initializing the vector to false as no
-        // vertex is visited at the beginning
-        vector<bool> visited(sizeOfV, false);
-        queue<int> q;
-        int start = getIndex(sr);
-        q.push(start);
-    
-        // Set source as visited
-        visited[start] = true;
-    
-        int vis;
-        while (!q.empty()) {
-            vis = q.front();
-    
-            // Print the current node
-            cout << vertices[vis] << " ";
-            q.pop();
-    
-            // For every adjacent vertex to the current vertex
+        
+        vector <bool> visitedDfs;
+        void dfs(char sr)
+        {
+            int start = getIndex(sr);
+            cout << vertices[start] << " ";
+            visitedDfs[start] = true;
             for (int i = 0; i <= sizeOfV; i++) {
-                if (edges[vis][i] == 1 && (!visited[i])) {
-    
-                    // Push the adjacent node to the queue
-                    q.push(i);
-    
-                    // Set
-                    visited[i] = true;
+
+                if (edges[start][i] == 1 && (!visitedDfs[i])) {
+                    dfs(vertices[i]);
                 }
             }
         }
-    }
-    
-    void dfs(char sr)
-    {
-        int start = getIndex(sr);
-        // Print the current node
-        cout << vertices[start] << " ";
-    
-        // Set current node as visited
-        visitedDfs[start] = true;
-    
-        // For every node of the graph
-        for (int i = 0; i <= sizeOfV; i++) {
-    
-            // If some node is adjacent to the current node
-            // and it has not already been visited
-            if (edges[start][i] == 1 && (!visitedDfs[i])) {
-                dfs(vertices[i]);
-            }
-        }
-    }
 };
 
 int main() {
@@ -122,23 +112,22 @@ int main() {
     gp.addVertex('C');
     gp.addVertex('D');
     gp.addVertex('E');
-    gp.addVertex('F');
-    gp.addVertex('G');
-    gp.addVertex('H');
-    gp.addVertex('I');
 
     gp.addEdge('A', 'B');
+    gp.addEdge('A', 'E');
     gp.addEdge('B', 'C');
-    gp.addEdge('C', 'F');
-    gp.addEdge('F', 'I');
-    gp.addEdge('D', 'A');
-    gp.addEdge('E', 'D');
-    gp.addEdge('G', 'D');
-    gp.addEdge('G', 'H');
-    gp.addEdge('E', 'H');
+    gp.addEdge('B', 'D');
+    gp.addEdge('C', 'D');
+    gp.addEdge('D', 'E');
+    gp.addEdge('A', 'E');
 
     gp.printMatrix();
+    cout<<endl<<endl;
+    cout<<"Breadth First Search"<<endl;
     gp.bfs('A');
-    cout<<endl;
+    cout<<endl<<endl;
+    cout<<"Depth First Search"<<endl;
     gp.dfs('A');
+
+    cout<<endl<<endl;
 }
